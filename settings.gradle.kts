77d346a9a87948ff66610c3db328aaf1fcc11daf
Shamil Magamadov <shamil.magamadov@twoday.com>
2023-07-26 11:41:42 +0200

    Init project

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "KotlinCICDToEC2Demo"
---
+++ src/main/kotlin/com/example/kotlincicdtoec2demo/KotlinCicdToEc2DemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlincicdtoec2demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinCicdToEc2DemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinCicdToEc2DemoApplication>(*args)
+}
