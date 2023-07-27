501e147afbf39d03dc06ae9ff0511ef4f2628a1a
Shamil Magamadov <shamil.magamadov@twoday.com>
2023-07-27 13:37:27 +0200

    change port

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,5 @@
 
 echo "Starting java jar"
 cd /home/ec2-user/server
-sudo java -jar -Dserver.port=80 *SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+sudo java -jar -Dserver.port=8000 *SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
 echo "Done starting java jar"
