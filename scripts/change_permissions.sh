90d4910b1651771a6dbb01cfa2eaad3d3a402c99
Shamil Magamadov <shamil.magamadov@twoday.com>
2023-07-27 12:36:08 +0200

    give read write permission to output.log

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 chmod +x /home/ec2-user/server/*.jar
-chmod +x /home/ec2-user/server/*_server.sh
-chmod +x /home/ec2-user/server/install_dependencies.sh
-chmod +x /home/ec2-user/server/change_permissions.sh
+chmod +x /home/ec2-user/server/*.sh
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,7 @@
 
 echo "Starting java jar"
 cd /home/ec2-user/server
-sudo java -jar -Dserver.port=8000 *SNAPSHOT.jar > output.log
+sudo touch output.log
+chmod u+rw output.log
+java -jar -Dserver.port=8000 *SNAPSHOT.jar > output.log
 echo "Done starting java jar"
