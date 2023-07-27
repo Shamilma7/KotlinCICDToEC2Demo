1c57e652ce9e68e06953776e76a18926107e8afc
Shamil Magamadov <shamil.magamadov@twoday.com>
2023-07-27 13:21:34 +0200

    update start server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,5 @@
 
 echo "Starting java jar"
 cd /home/ec2-user/server
-sudo nohup java -jar -Dserver.port=80 *SNAPSHOT.jar > output.log &
+sudo nohup java -jar -Dserver.port=80 *SNAPSHOT.jar &
 echo "Done starting java jar"
