b9d52ccc285f17973d27a57ba0557a10ddddff73
Shamil Magamadov <shamil.magamadov@twoday.com>
2023-07-27 12:42:15 +0200

    make sudo script

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-chmod +x /home/ec2-user/server/*.jar
-chmod +x /home/ec2-user/server/*.sh
+sudo chmod +x /home/ec2-user/server/*.jar
+sudo chmod +x /home/ec2-user/server/*.sh
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,7 +2,5 @@
 
 echo "Starting java jar"
 cd /home/ec2-user/server
-sudo touch output.log
-chmod u+rw output.log
-java -jar -Dserver.port=8000 *SNAPSHOT.jar > output.log
+sudo java -jar -Dserver.port=8000 *SNAPSHOT.jar > output.log
 echo "Done starting java jar"
