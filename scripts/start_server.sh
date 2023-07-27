8cf68100db31e5413a4c76545f288750767bb3ab
Shamil Magamadov <shamil.magamadov@twoday.com>
2023-07-27 12:55:37 +0200

    fix

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,5 @@
 
 echo "Starting java jar"
 cd /home/ec2-user/server
-sudo java -jar -Dserver.port=8000 *SNAPSHOT.jar > output.log
+sudo java -jar -Dserver.port=8000 *SNAPSHOT.jar > output.log &
 echo "Done starting java jar"
