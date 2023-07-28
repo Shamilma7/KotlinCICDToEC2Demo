9abf08eac1598e6e9963e813dfcd30a9fc02fc45
Shamil Magamadov <shamil.magamadov@twoday.com>
2023-07-28 15:33:08 +0200

    permit cross origin

--- src/main/kotlin/com/example/kotlincicdtoec2demo/controller/AWSController.kt
+++ src/main/kotlin/com/example/kotlincicdtoec2demo/controller/AWSController.kt
@@ -4,10 +4,12 @@ import com.example.kotlincicdtoec2demo.entity.Teacher
 import com.example.kotlincicdtoec2demo.service.TeacherService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin
 class AWSController {
 
     @Autowired
